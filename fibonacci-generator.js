530aaf45b03e2e2a9d07ce2b5147b6b5ee3aa34d
michaelbulaongmusic <115378265+michaelbulaongmusic@users.noreply.github.com>
2022-10-24 17:20:29 +0800

    Create fibonacci-generator.js

---
+++ fibonacci-generator.js
@@ -0,0 +1,17 @@
+var output = [];
+
+function fibonacciGenerator(n) {
+
+    for (var i = 0; i < n; i++) {
+
+        if (i < 2) {
+            output.push(i);
+        }
+        else {
+            output.push(output[i-1] + output[i-2]);
+        }
+            
+    }
+    
+    console.log(output);
+}
