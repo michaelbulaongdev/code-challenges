ee27697aafcc6e13d1e506cf79212709e5d538af
michaelbulaongmusic <115378265+michaelbulaongmusic@users.noreply.github.com>
2022-10-25 08:43:14 +0800

    Create constructor-function.js

---
+++ constructor-function.js
@@ -0,0 +1,12 @@
+function HouseKeeper (name, age, hasWorkPermit, yearsXP, skills) {
+    this.name = name;
+    this.age = age;
+    this.hasWorkPermit = hasWorkPermit;
+    this.yearsXP = yearsXP;
+    this.skills = skills;
+}
+
+var houseKeeper1 = new HouseKeeper("Brenda", 18, true, 5, ["cook", "clean", "babysit"]);
+
+
+// reference: https://www.w3schools.com/js/js_object_constructors.asp
