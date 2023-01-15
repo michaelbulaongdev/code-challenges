499a089bcb4988fc1606f30a4a5b13d966dd0728
michaelbulaongdev <michaelbulaongdev@gmail.com>
2023-01-15 15:17:07 +0800

    Create addTo.js
    a function that accepts a number as a parameter and adds all natural numbers smaller or equal than the parameter

---
+++ addTo.js
@@ -0,0 +1,13 @@
+function addTo(x) {
+
+let sum = 0
+
+for (let i=0; i<=x; i++) {
+  sum = sum + i
+}
+
+return sum
+
+}
+
+//a function that accepts a number as a parameter and adds all natural numbers smaller or equal than the parameter
