225a6e7c6bd830411600aa9b062233d826e9882b
michaelbulaongdev <michaelbulaongdev@gmail.com>
2023-01-15 15:10:06 +0800

    Create spaceMaker.js
    Write a function that takes a natural number n and returns a string of n spaces.

---
+++ spaceMaker.js
@@ -0,0 +1,13 @@
+function spaces(n) {
+
+let result = '';
+
+while (result.length < n) {
+  result = result + ' ';
+}
+
+return result;
+
+}
+
+//Write a function that takes a natural number n and returns a string of n spaces.
