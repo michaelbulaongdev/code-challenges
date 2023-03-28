f0e7ea58aa5ce30e356f353428b767ebff2e1011
michaelbulaongdev <michaelbulaongdev@gmail.com>
2023-03-28 10:46:48 +0800

    Update leastNumSteps.js

--- leastNumSteps.js
+++ leastNumSteps.js
@@ -1,28 +1,33 @@
 //given a grid with 3 rows and 8 columns
-//create a function that calculates least number of steps from pointA to pointB
-//movement allowed: down and diagonals, cant move up, left and right
-//points given as coordinates along (x, y) axis where point A is the start and point X is the end.
+//create a function that calculates the least number of steps from pointA to pointX
+//movement allowed: down and diagonals, cannot move up, left and right
+//arguments must be whole number coordinates: pointA(Ax,Ay) pointX(Xx,Xy)
+//range of Ax and Xx: 1-8
+//range of Ay and Xy: 1-3
 
-function leastNumSteps(a1, a2, x1, x2) {
-  //check if type of inputs are numbers
+function leastNumSteps(Ax, Ay, Xx, Xy) {
+  //check if inputs are numbers
   if (
-    typeof a1 !== "number" ||
-    typeof a2 !== "number" ||
-    typeof x1 !== "number" ||
-    typeof x2 !== "number"
+    typeof Ax !== "number" ||
+    typeof Ay !== "number" ||
+    typeof Xx !== "number" ||
+    typeof Xy !== "number"
   ) {
-    return `invalid input`;
+    return `invalid input: must be number type`;
   }
 
-  //round down to nearest whole numbers, removing decimals
-  let Ax = Math.floor(a1);
-  let Ay = Math.floor(a2);
-  let Xx = Math.floor(x1);
-  let Xy = Math.floor(x2);
+  //check if inputs are whole numbers
+  if (
+    Ax - Math.floor(Ax) !== 0 ||
+    Ay - Math.floor(Ay) !== 0 ||
+    Xx - Math.floor(Xx) !== 0 ||
+    Xy - Math.floor(Xy) !== 0
+  ) {
+    return `invalid input: must be whole numbers`;
+  }
 
-  //check if source and destination are in the grid
+  //check if inputs are inside the grid
   let insideGrid = false;
-
   if (Ax >= 1 && Ax <= 8) {
     if (Ay >= 1 && Ay <= 3) {
       if (Xx >= 1 && Xx <= 8) {
@@ -32,12 +37,13 @@ function leastNumSteps(a1, a2, x1, x2) {
       }
     }
   }
-
-  if (insideGrid === false) {
-    return `input is outside of the grid`;
+  if (!insideGrid) {
+    return `invalid input: outside of grid`;
   }
 
+  //========================
   //main logic: calculate least steps from startPoint to endPoint
+  //========================
 
   let steps = 0;
 
@@ -47,20 +53,44 @@ function leastNumSteps(a1, a2, x1, x2) {
     [13, 23, 33, 43, 53, 63, 73, 83],
   ];
 
-  //convert point data to array format grid[rowIndex][colIndex]
+  //convert coordinates to grid[rowIndex][colIndex]
   const startPoint = grid[Ay - 1][Ax - 1];
   const endPoint = grid[Xy - 1][Xx - 1];
 
   console.log(`from A(${Ax},${Ay}) to X(${Xx},${Xy})`);
-  console.log(`from grid points ${startPoint} to ${endPoint}`);
+  console.log(`grid: from ${startPoint} to ${endPoint}`);
 
-  //check position of endpoint relative to startpoint
+  //if X and A are on the same y-axis
+  if (Xx === Ax) {
+    //if X and A are the same
+    if (Xy === Ay) {
+      //do nothing
+    }
+    //if X is directly below A
+    else if (Xy > Ay) {
+      for (let i = Ay - 1; i < Xy - 1; i++) {
+        steps = steps + 1;
+      }
+    }
+    //if X is directly above A
+    else if (Xy < Ay) {
+      if (Ay - Xy === 1) {
+        steps = 3;
+      } else {
+        steps = 2;
+      }
+    }
+  }
+
+  //if X is to the right of A
+  if (Xx > Ax) {
+  }
 
   return `${steps} steps`;
 }
 
-//from start pointA(3, 2) to end pointX(8, 1)
-console.log(leastNumSteps(3, 2, 8, 1)); //expected output: 5 steps
+//from testing
+console.log(leastNumSteps(4, 2, 4, 1));
 
 //validation test
 // console.log(leastNumSteps(2, 2, 9, 4)); //input is outside of the grid
