60c6df0ef30e15485096996278aea7ae8515e0ed
michaelbulaongdev <michaelbulaongdev@gmail.com>
2023-03-28 22:41:47 +0800

    Update leastNumSteps.js

--- leastNumSteps.js
+++ leastNumSteps.js
@@ -6,87 +6,97 @@
 //range of Ay and Xy: 1-3
 
 function leastNumSteps(Ax, Ay, Xx, Xy) {
-  //check if inputs are numbers
-  if (
-    typeof Ax !== "number" ||
-    typeof Ay !== "number" ||
-    typeof Xx !== "number" ||
-    typeof Xy !== "number"
-  ) {
-    return `invalid input: must be number type`;
-  }
+	//check if inputs are numbers
+	if (
+		typeof Ax !== 'number' ||
+		typeof Ay !== 'number' ||
+		typeof Xx !== 'number' ||
+		typeof Xy !== 'number'
+	) {
+		return `invalid input: must be number type`;
+	}
 
-  //check if inputs are whole numbers
-  if (
-    Ax - Math.floor(Ax) !== 0 ||
-    Ay - Math.floor(Ay) !== 0 ||
-    Xx - Math.floor(Xx) !== 0 ||
-    Xy - Math.floor(Xy) !== 0
-  ) {
-    return `invalid input: must be whole numbers`;
-  }
+	//check if inputs are whole numbers
+	if (
+		Ax - Math.floor(Ax) !== 0 ||
+		Ay - Math.floor(Ay) !== 0 ||
+		Xx - Math.floor(Xx) !== 0 ||
+		Xy - Math.floor(Xy) !== 0
+	) {
+		return `invalid input: must be whole numbers`;
+	}
 
-  //check if inputs are inside the grid
-  let insideGrid = false;
-  if (Ax >= 1 && Ax <= 8) {
-    if (Ay >= 1 && Ay <= 3) {
-      if (Xx >= 1 && Xx <= 8) {
-        if (Xy >= 1 && Xy <= 3) {
-          insideGrid = true;
-        }
-      }
-    }
-  }
-  if (!insideGrid) {
-    return `invalid input: outside of grid`;
-  }
+	//check if inputs are inside the grid
+	let insideGrid = false;
+	if (Ax >= 1 && Ax <= 8) {
+		if (Ay >= 1 && Ay <= 3) {
+			if (Xx >= 1 && Xx <= 8) {
+				if (Xy >= 1 && Xy <= 3) {
+					insideGrid = true;
+				}
+			}
+		}
+	}
+	if (!insideGrid) {
+		return `invalid input: outside of grid`;
+	}
 
-  //========================
-  //main logic: calculate least steps from startPoint to endPoint
-  //========================
+	//initial values
+	let steps = 0;
 
-  let steps = 0;
+	const grid = [
+		[11, 21, 31, 41, 51, 61, 71, 81],
+		[12, 22, 32, 42, 52, 62, 72, 82],
+		[13, 23, 33, 43, 53, 63, 73, 83],
+	];
 
-  const grid = [
-    [11, 21, 31, 41, 51, 61, 71, 81],
-    [12, 22, 32, 42, 52, 62, 72, 82],
-    [13, 23, 33, 43, 53, 63, 73, 83],
-  ];
+	//convert coordinates to grid[rowIndex][colIndex]
+	const startPoint = grid[Ay - 1][Ax - 1];
+	const endPoint = grid[Xy - 1][Xx - 1];
 
-  //convert coordinates to grid[rowIndex][colIndex]
-  const startPoint = grid[Ay - 1][Ax - 1];
-  const endPoint = grid[Xy - 1][Xx - 1];
+	console.log(`from A(${Ax},${Ay}) to X(${Xx},${Xy})`);
+	console.log(`grid: from ${startPoint} to ${endPoint}`);
 
-  console.log(`from A(${Ax},${Ay}) to X(${Xx},${Xy})`);
-  console.log(`grid: from ${startPoint} to ${endPoint}`);
+	//========================
+	//main logic: calculate least steps from startPoint to endPoint
+	//========================
 
-  //if X and A are on the same y-axis
-  if (Xx === Ax) {
-    //if X and A are the same
-    if (Xy === Ay) {
-      //do nothing
-    }
-    //if X is directly below A
-    else if (Xy > Ay) {
-      for (let i = Ay - 1; i < Xy - 1; i++) {
-        steps = steps + 1;
-      }
-    }
-    //if X is directly above A
-    else if (Xy < Ay) {
-      if (Ay - Xy === 1) {
-        steps = 3;
-      } else {
-        steps = 2;
-      }
-    }
-  }
+	//if X and A are on the same y-axis
+	if (Xx === Ax) {
+		//if X and A are the same
+		if (Xy === Ay) {
+			//do nothing
+		}
+		//if X is directly below A
+		else if (Xy > Ay) {
+			for (let i = Ay - 1; i < Xy - 1; i++) {
+				steps = steps + 1;
+			}
+		}
+		//if X is directly above A
+		else if (Xy < Ay) {
+			if (Ay - Xy === 1) {
+				steps = 3;
+			} else {
+				steps = 2;
+			}
+		}
+		//destination reached!
+		Ay = Xy;
+	}
 
-  //if X is to the right of A
-  if (Xx > Ax) {
-  }
+	//if X is to the right of A
+	if (Xx > Ax) {
+		//if same y-axis
+		if (Xy === Ay) {
+			if (Xx - Ax === 1) {
+			}
+		}
+		//if above
+		//if below
+	}
 
-  return `${steps} steps`;
+	return `${steps} steps`;
 }
 
 //from testing
